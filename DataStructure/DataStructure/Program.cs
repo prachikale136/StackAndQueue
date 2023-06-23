eb1253909185dd67f1c9a3153afd1abab4402883
Prachi <prachikale136@gmail.com>
2023-06-23 22:23:30 +0530

    [Prachi] Add:ability to create satck

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello, World!");
+            StackAndQueue stackLinkedList = new StackAndQueue();
+            stackLinkedList.Push(56);
+            stackLinkedList.Push(30);
+            stackLinkedList.Push(70);
+            stackLinkedList.Display();
+        }
+    }
+}
